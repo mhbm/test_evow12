8f9fc60aa629c7be6b05138d04e0ee99ececb062
Mateus Macedo <mhbassomacedo@gmail.com>
2019-10-15 19:33:14 -0300

    Update files

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,14 +1,35 @@
 function addCadastro() {
-    var pessoa = JSON.stringify({
-        name: $("#txtName").val(),
-        email: $("#txtEmail").val(),
-        password: $("#txtPassword").val()
-    });
-    console.log("pessoa:", pessoa)
-    localStorage.setItem("pessoa", pessoa);
-    //alert("Registro adicionado.");
-    //alert(localStorage.getItem(pessoa));
-    location.href = 'result.html';
+
+    if (!validadePassword()) {
+        alert("Senha precisa ter no mínimo oito caracteres");
+        return;
+    }
+    if (validadeEmail()) {
+
+        var pessoa = JSON.stringify({
+            name: $("#txtName").val(),
+            email: $("#txtEmail").val(),
+            password: $("#txtPassword").val()
+        });
+        console.log("pessoa:", pessoa)
+        localStorage.setItem("pessoa", pessoa);
+        //alert("Registro adicionado.");
+        //alert(localStorage.getItem(pessoa));
+        location.href = 'result.html';
+    } else {
+        alert("Email inválido!");
+    }
+}
+
+function validadePassword() {
+    return $("#txtPassword").val().length > 7
+}
+
+function validadeEmail() {
+    var email = $("#txtEmail").val();
+
+    return /\S+@\S+\.\S+/.test(email);
+
 }
 
 $(document).ready(function() {
